036153b28efdc1bc8e5ea441523a0e3feda11c0d
barteldvn <barteldvn@gmail.com>
2023-10-26 08:01:12 +0200

    getTrain and getTrainTimes

--- src/main/java/be/kuleuven/distributedsystems/cloud/controller/BookingRestController.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/controller/BookingRestController.java
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.annotation.Resource;
+import java.rmi.RemoteException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class BookingRestController {
@@ -42,19 +40,31 @@ public class BookingRestController {
                 .getContent();
     }
 
-//    @GetMapping("/api/getTrain/{trainCompany}/{trainId}")
-//    Train getTrain(@PathVariable String trainCompany, @PathVariable String trainId) {
-//        Optional<Meal> meal = mealsRepository.findMeal(id);
-//
-//        return meal.orElseThrow(() -> new MealNotFoundException(id));
-//    }
-//
-//    @GetMapping("/api/getTrainTimes/{trainCompany}/{trainId}}")
-//    Collection<LocalDateTime> getTrainTimes(@PathVariable String trainCompany, @PathVariable String trainId) {
-//        Optional<Meal> meal = mealsRepository.findMeal(id);
-//
-//        return meal.orElseThrow(() -> new MealNotFoundException(id));
-//    }
+    @GetMapping("/api/getTrain/{trainCompany}/{trainId}")
+    Train getTrain(@PathVariable String trainCompany, @PathVariable String trainId) throws RemoteException {
+        Collection<Train> trains =  getAllTrains();
+        for (Train train : trains){
+            if (Objects.equals(train.getTrainCompany(), trainCompany) && Objects.equals(train.getTrainId().toString(), trainId)) return train;
+        }
+        throw new RemoteException("Train not found");
+    }
+
+    @GetMapping("/api/getTrainTimes/{trainCompany}/{trainId}}")
+    Collection<LocalDateTime> getTrainTimes(@PathVariable String trainCompany, @PathVariable String trainId) throws RemoteException {
+        Train train =  getTrain(trainCompany, trainId);
+        return webClientBuilder
+                .baseUrl("https://reliabletrains.com")
+                .build()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .pathSegment("trains/{id}/times")
+                        .queryParam("key", API_KEY)
+                        .build(train.getTrainId()))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<CollectionModel<LocalDateTime>>() {})
+                .block()
+                .getContent();
+    }
 //
 //
 //    @GetMapping("/api/getAvailableSeats/{trainCompany}/{trainId}/{time}")
